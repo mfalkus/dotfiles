159ca81078a57898676c4ac3d4982225836775db
Martin Falkus <mf@netcraft.com>
2014-08-20 08:52:20 +0100

    Copy gitconfig in to place.
    The only reason I don't symlink is because the email details often get
    changed from machine to machine, so working from a base copy is a
    good approach.

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -4,6 +4,7 @@ dir=`pwd`
 # list of files/folders to symlink in homedir
 # note, don't copy .gitconfig by default
 files=".bashrc .vimrc .ctags .tmux.conf"
+cp_files=".gitconfig"
 
 # change to the dotfiles directory
 cd ~
@@ -13,4 +14,9 @@ for file in $files; do
     unlink ~/$file
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/$file
+done
+
+for file in $cp_files; do
+    echo "Copying $file (+ making backup if required)"
+    cp -b $dir/$file ~/$file
 done
